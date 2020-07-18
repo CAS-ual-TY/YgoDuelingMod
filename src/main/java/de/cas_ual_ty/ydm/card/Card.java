cb638b2d025fc38525327d002c0da20abeb3f7b6
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-07-18 12:53:42 +0200

    Added default Card implementation for testing

--- src/main/java/de/cas_ual_ty/ydm/card/Card.java
+++ src/main/java/de/cas_ual_ty/ydm/card/Card.java
@@ -1,11 +1,29 @@
 package de.cas_ual_ty.ydm.card;
 
+import com.google.gson.JsonObject;
+
 import de.cas_ual_ty.ydm.card.properties.Properties;
+import de.cas_ual_ty.ydm.util.Database;
+import de.cas_ual_ty.ydm.util.JsonKeys;
+import net.minecraft.nbt.CompoundNBT;
 
 public class Card
 {
     public Properties properties;
-    public String picture;
+    public byte pictureIndex;
     public String setId;
     public Rarity rarity;
+    
+    public Card(JsonObject j)
+    {
+        
+    }
+    
+    public Card(Properties p)
+    {
+        this.properties = p;
+        this.pictureIndex = 0;
+        this.setId = "SET_ID";
+        this.rarity = Rarity.COMMON;
+    }
 }
