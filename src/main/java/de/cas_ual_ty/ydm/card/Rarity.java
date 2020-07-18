89b4e931b4a9a5751147d60defe512ca194c0dd4
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-07-18 14:32:27 +0200

    Extended Rarity enum with helper methods

--- src/main/java/de/cas_ual_ty/ydm/card/Rarity.java
+++ src/main/java/de/cas_ual_ty/ydm/card/Rarity.java
@@ -2,5 +2,27 @@ package de.cas_ual_ty.ydm.card;
 
 public enum Rarity
 {
-    COMMON;
+    COMMON("Common");
+    
+    public final String name;
+    
+    private Rarity(String name)
+    {
+        this.name = name;
+    }
+    
+    public static final Rarity[] VALUES = Rarity.values();
+    
+    public static Rarity fromString(String s)
+    {
+        for(Rarity rarity : Rarity.VALUES)
+        {
+            if(rarity.name.equals(s))
+            {
+                return rarity;
+            }
+        }
+        
+        return null;
+    }
 }
