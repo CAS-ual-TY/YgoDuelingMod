c31c74140d380ce97677dd04048b3203ecc5087a
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-07-19 10:37:54 +0200

    AGAIN forgot to commit smth to previous commit (setup method in proxy)

--- src/main/java/de/cas_ual_ty/ydm/proxy/ClientProxy.java
+++ src/main/java/de/cas_ual_ty/ydm/proxy/ClientProxy.java
@@ -1,6 +1,15 @@
 package de.cas_ual_ty.ydm.proxy;
 
+import de.cas_ual_ty.ydm.YDM;
+import de.cas_ual_ty.ydm.util.YDMResourcePackFinder;
+import de.cas_ual_ty.ydm.util.YDMUtil;
+import net.minecraft.client.Minecraft;
+
 public class ClientProxy implements ISidedProxy
 {
-    
+    @Override
+    public void setup()
+    {
+        Minecraft.getInstance().getResourcePackList().addPackFinder(new YDMResourcePackFinder(YDM.cardImagesFolder, YDMUtil.PNG_FILTER));
+    }
 }
--- src/main/java/de/cas_ual_ty/ydm/proxy/ISidedProxy.java
+++ src/main/java/de/cas_ual_ty/ydm/proxy/ISidedProxy.java
@@ -2,5 +2,8 @@ package de.cas_ual_ty.ydm.proxy;
 
 public interface ISidedProxy
 {
-    
+    public default void setup()
+    {
+        
+    }
 }
