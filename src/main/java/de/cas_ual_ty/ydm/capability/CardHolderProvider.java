597e8347309be410732f317b39517ec951e89fb1
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-07-19 10:37:19 +0200

    Forgot to rename a variable when copying code from other repo

--- src/main/java/de/cas_ual_ty/ydm/capability/CardHolderProvider.java
+++ src/main/java/de/cas_ual_ty/ydm/capability/CardHolderProvider.java
@@ -10,31 +10,31 @@ import net.minecraftforge.common.util.LazyOptional;
 public class CardHolderProvider implements ICapabilitySerializable<INBT>
 {
     @CapabilityInject(value = ICardHolder.class)
-    public static final Capability<ICardHolder> CAPABILITY_PRINT_HOLDER = null;
+    public static final Capability<ICardHolder> CAPABILITY_CARD_HOLDER = null;
     
     private final LazyOptional<ICardHolder> instance;
     
     public CardHolderProvider()
     {
-        this.instance = LazyOptional.of(CardHolderProvider.CAPABILITY_PRINT_HOLDER::getDefaultInstance);
+        this.instance = LazyOptional.of(CardHolderProvider.CAPABILITY_CARD_HOLDER::getDefaultInstance);
     }
     
     @Override
     public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side)
     {
-        return cap == CardHolderProvider.CAPABILITY_PRINT_HOLDER ? this.instance.cast() : LazyOptional.empty();
+        return cap == CardHolderProvider.CAPABILITY_CARD_HOLDER ? this.instance.cast() : LazyOptional.empty();
     }
     
     @Override
     public INBT serializeNBT()
     {
-        return CardHolderProvider.CAPABILITY_PRINT_HOLDER.getStorage().writeNBT(CardHolderProvider.CAPABILITY_PRINT_HOLDER, this.instance.orElseThrow(() -> new IllegalArgumentException("LazyOptional must not be empty!")), null);
+        return CardHolderProvider.CAPABILITY_CARD_HOLDER.getStorage().writeNBT(CardHolderProvider.CAPABILITY_CARD_HOLDER, this.instance.orElseThrow(() -> new IllegalArgumentException("LazyOptional must not be empty!")), null);
     }
     
     @Override
     public void deserializeNBT(INBT nbt)
     {
-        CardHolderProvider.CAPABILITY_PRINT_HOLDER.getStorage().readNBT(CardHolderProvider.CAPABILITY_PRINT_HOLDER, this.instance.orElseThrow(() -> new IllegalArgumentException("LazyOptional must not be empty!")), null, nbt);
+        CardHolderProvider.CAPABILITY_CARD_HOLDER.getStorage().readNBT(CardHolderProvider.CAPABILITY_CARD_HOLDER, this.instance.orElseThrow(() -> new IllegalArgumentException("LazyOptional must not be empty!")), null, nbt);
     }
     
     public Runnable getListener()
