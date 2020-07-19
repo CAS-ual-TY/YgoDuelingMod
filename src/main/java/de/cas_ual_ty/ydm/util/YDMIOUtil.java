f83b4905a58db645a83789438f27b9c52be5271b
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-07-19 16:19:58 +0200

    Expanded the ydm io utils file

--- src/main/java/de/cas_ual_ty/ydm/util/YDMIOUtil.java
+++ src/main/java/de/cas_ual_ty/ydm/util/YDMIOUtil.java
@@ -1,5 +1,6 @@
 package de.cas_ual_ty.ydm.util;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -10,21 +11,28 @@ public class YdmIOUtil
 {
     public static final FileFilterSuffix JSON_FILTER = YdmIOUtil.createFileFilter(".json");
     public static final FileFilterSuffix PNG_FILTER = YdmIOUtil.createFileFilter(".png");
-
+    
     public static FileFilterSuffix createFileFilter(String requiredSuffix)
     {
         return () -> requiredSuffix;
     }
-
-    public static void downloadFile(URL url, String fileName) throws IOException
+    
+    public static void downloadFile(URL url, File target) throws IOException
     {
         InputStream in = url.openStream();
-        Files.copy(in, Paths.get(fileName));
+        Files.copy(in, Paths.get(target.toURI()));
     }
-
+    
     public static void setAgent()
     {
         System.setProperty("http.agent", "Netscape 1.0");
     }
     
+    public static void createDirIfNonExistant(File file)
+    {
+        if(!file.exists() && file.isDirectory())
+        {
+            file.mkdir();
+        }
+    }
 }
