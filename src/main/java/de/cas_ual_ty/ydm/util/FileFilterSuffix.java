73ad3e396e1946a082cd73004546aa1a8eba0a59
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-07-19 10:38:30 +0200

    Created some file filters

---
+++ src/main/java/de/cas_ual_ty/ydm/util/FileFilterSuffix.java
@@ -0,0 +1,15 @@
+package de.cas_ual_ty.ydm.util;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public interface FileFilterSuffix extends FileFilter
+{
+    public String getRequiredSuffix();
+    
+    @Override
+    default boolean accept(File f)
+    {
+        return f.isFile() && f.getName().toLowerCase().endsWith(this.getRequiredSuffix());
+    }
+}
