76bcbb10665372e90b43e7f256e8757f591afda3
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-07-30 15:36:24 +0200

    Added all deck boxes (non functional)

--- src/main/java/de/cas_ual_ty/ydm/YdmItems.java
+++ src/main/java/de/cas_ual_ty/ydm/YdmItems.java
@@ -3,6 +3,7 @@ package de.cas_ual_ty.ydm;
 import de.cas_ual_ty.ydm.binder.CardBinderItem;
 import de.cas_ual_ty.ydm.card.CardItem;
 import de.cas_ual_ty.ydm.card.CosmeticItem;
+import de.cas_ual_ty.ydm.deckbox.DeckBoxItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.Properties;
@@ -23,6 +24,27 @@ public class YdmItems
     public static final CardBinderItem CARD_BINDER = null;
     public static final Item SIMPLE_DUEL_BLOCK = null;
     
+    public static final DeckBoxItem BLACK_DECK_BOX = null;
+    public static final DeckBoxItem RED_DECK_BOX = null;
+    public static final DeckBoxItem GREEN_DECK_BOX = null;
+    public static final DeckBoxItem BROWN_DECK_BOX = null;
+    public static final DeckBoxItem BLUE_DECK_BOX = null;
+    public static final DeckBoxItem PURPLE_DECK_BOX = null;
+    public static final DeckBoxItem CYAN_DECK_BOX = null;
+    public static final DeckBoxItem LIGHT_GRAY_DECK_BOX = null;
+    public static final DeckBoxItem GRAY_DECK_BOX = null;
+    public static final DeckBoxItem PINK_DECK_BOX = null;
+    public static final DeckBoxItem LIME_DECK_BOX = null;
+    public static final DeckBoxItem YELLOW_DECK_BOX = null;
+    public static final DeckBoxItem LIGHT_BLUE_DECK_BOX = null;
+    public static final DeckBoxItem MAGENTA_DECK_BOX = null;
+    public static final DeckBoxItem ORANGE_DECK_BOX = null;
+    public static final DeckBoxItem WHITE_DECK_BOX = null;
+    public static final DeckBoxItem IRON_DECK_BOX = null;
+    public static final DeckBoxItem GOLD_DECK_BOX = null;
+    public static final DeckBoxItem DIAMOND_DECK_BOX = null;
+    public static final DeckBoxItem EMERALD_DECK_BOX = null;
+    
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
@@ -32,5 +54,25 @@ public class YdmItems
         registry.register(new CardItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "card"));
         registry.register(new CardBinderItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "card_binder"));
         registry.register(new BlockItem(YdmBlocks.SIMPLE_DUEL_BLOCK, new Properties().group(YDM.ydmItemGroup)).setRegistryName(YDM.MOD_ID, "simple_duel_block"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "black_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "red_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "green_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "brown_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "blue_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "purple_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "cyan_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "light_gray_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "gray_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "pink_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "lime_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "yellow_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "light_blue_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "magenta_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "orange_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "white_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "iron_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "gold_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "diamond_deck_box"));
+        registry.register(new DeckBoxItem(new Properties().group(YDM.ydmItemGroup).maxStackSize(1)).setRegistryName(YDM.MOD_ID, "emerald_deck_box"));
     }
 }
---
+++ src/main/java/de/cas_ual_ty/ydm/deckbox/DeckBoxItem.java
@@ -0,0 +1,11 @@
+package de.cas_ual_ty.ydm.deckbox;
+
+import net.minecraft.item.Item;
+
+public class DeckBoxItem extends Item
+{
+    public DeckBoxItem(Properties properties)
+    {
+        super(properties);
+    }
+}
