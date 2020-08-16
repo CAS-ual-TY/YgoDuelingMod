8e1389c189bd2f2320414e82d010cd1b67cc2d8a
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2020-08-16 17:16:55 +0200

    Added new prep duel state

--- src/main/java/de/cas_ual_ty/ydm/duel/DuelState.java
+++ src/main/java/de/cas_ual_ty/ydm/duel/DuelState.java
@@ -2,7 +2,15 @@ package de.cas_ual_ty.ydm.duel;
 
 public enum DuelState
 {
-    IDLE, DUELING, SIDING, END;
+    IDLE, PREPARING, DUELING, SIDING, END;
+    
+    /*
+     * Idle: Nothing there, role selection open
+     * Preparing: p1 chooses format, p1 chooses banlist, choose deck, ready up
+     * Dueling: duh
+     * Siding: duh
+     * End: Match ended, winner is shown, add "rematch" and "new duel" button
+     */
     
     public static final DuelState[] VALUES = DuelState.values();
     
