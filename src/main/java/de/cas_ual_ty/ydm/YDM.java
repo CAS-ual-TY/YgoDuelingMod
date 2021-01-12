6d41087b0560234c5a8b96dcc5c8dada4859f14e
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2021-01-12 14:57:27 +0100

    Remove old debugging method

--- src/main/java/de/cas_ual_ty/ydm/YDM.java
+++ src/main/java/de/cas_ual_ty/ydm/YDM.java
@@ -1,14 +1,12 @@
 package de.cas_ual_ty.ydm;
 
 import java.io.File;
-import java.util.LinkedList;
 import java.util.Random;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import de.cas_ual_ty.ydm.card.properties.Properties;
 import de.cas_ual_ty.ydm.cardbinder.CardBinderCardsManager;
 import de.cas_ual_ty.ydm.cardbinder.CardBinderMessages;
 import de.cas_ual_ty.ydm.cardinventory.JsonCardsManager;
@@ -345,27 +343,6 @@ public class YDM
         else
         {
             YDM.debug(s.toString());
-        }
-    }
-    
-    // For debugging
-    // call this at the end of initFiles
-    @Deprecated
-    public static void scanCardsForErrors()
-    {
-        for(Properties p : YdmDatabase.PROPERTIES_LIST)
-        {
-            try
-            {
-                p.addInformation(new LinkedList<>());
-            }
-            catch (RuntimeException e)
-            {
-                YDM.debug("Failed showing card information for:");
-                YDM.debug(p.getName());
-                YDM.debug(String.valueOf(p.getId()));
-                e.printStackTrace();
-            }
         }
     }
 }
