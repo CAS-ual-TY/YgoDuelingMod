8637b1f11324b266adfbb4eec5e1334a1ead0793
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2021-01-26 00:11:21 +0100

    Further improved card pulling code

--- src/main/java/de/cas_ual_ty/ydm/set/DistributionCardPuller.java
+++ src/main/java/de/cas_ual_ty/ydm/set/DistributionCardPuller.java
@@ -35,6 +35,12 @@ public class DistributionCardPuller extends CardPuller
     }
     
     @Override
+    public void postDBInit()
+    {
+        super.postDBInit();
+    }
+    
+    @Override
     public List<ItemStack> open(Random random)
     {
         List<ItemStack> list = new ArrayList<>();
@@ -50,43 +56,17 @@ public class DistributionCardPuller extends CardPuller
     
     public List<CardHolder> openDistribution(Random random)
     {
-        int weightViewed = 0;
-        int x = random.nextInt(this.distribution.totalWeight);
-        
-        Pull pull = null;
-        
-        for(Pull p : this.distribution.pulls)
-        {
-            if(weightViewed + p.weight > x)
-            {
-                pull = p;
-                break;
-            }
-            
-            weightViewed += p.weight;
-        }
+        Pull pull = this.choosePull(random);
         
         if(pull != null)
         {
-            ArrayList<CardHolder> cards = new ArrayList<>();
+            List<CardHolder> cards = new ArrayList<>();
             
-            ArrayList<CardHolder> cardPool;
+            List<CardHolder> cardPool;
             
             for(PullEntry pe : pull.pullEntries)
             {
-                cardPool = new ArrayList<>(this.set.cards.size() * pe.rarities.length);
-                cardPool.sort((ch1, ch2) -> Long.compare(ch1.getCard().getId(), ch2.getCard().getId()));
-                
-                for(String rarity : pe.rarities)
-                {
-                    for(CardHolder c : this.set.cards)
-                    {
-                        if(c.getRarity().equals(rarity))
-                        {
-                            cardPool.add(c);
-                        }
-                    }
-                }
+                cardPool = this.makeCardPool(random, pe);
                 
                 if(cardPool.size() <= 0)
                 {
@@ -100,6 +80,44 @@ public class DistributionCardPuller extends CardPuller
         }
         
         return null;
+    }
+    
+    protected Pull choosePull(Random random)
+    {
+        int weightViewed = 0;
+        int x = random.nextInt(this.distribution.totalWeight);
+        
+        for(Pull p : this.distribution.pulls)
+        {
+            if(weightViewed + p.weight > x)
+            {
+                return p;
+            }
+            
+            weightViewed += p.weight;
+        }
+        
+        return null;
+    }
+    
+    protected List<CardHolder> makeCardPool(Random random, PullEntry pe)
+    {
+        List<CardHolder> cardPool = new ArrayList<>(this.set.cards.size() * pe.rarities.length);
+        
+        for(String rarity : pe.rarities)
+        {
+            for(CardHolder c : this.set.cards)
+            {
+                if(c.getRarity().equals(rarity))
+                {
+                    cardPool.add(c);
+                }
+            }
+        }
+        
+        cardPool.sort((ch1, ch2) -> Long.compare(ch1.getCard().getId(), ch2.getCard().getId()));
+        
+        return cardPool;
     }
     
     protected void chooseCardsFromPool(Random random, List<CardHolder> cardPool, PullEntry pe, List<CardHolder> cards)
@@ -116,20 +134,10 @@ public class DistributionCardPuller extends CardPuller
         // First we count the amount of unique cards
         // (so the same card but with different rarities only counts once)
         // The cardPool list is sorted by card IDs
-        int uniqueIds = 0;
-        Properties lastCard = null;
-        
-        for(CardHolder ch : cardPool)
-        {
-            if(ch.getCard() != lastCard)
-            {
-                lastCard = ch.getCard();
-                ++uniqueIds;
-            }
-        }
+        int uniqueCards = countUniqueCards(cardPool);
         
         // Now we check if there enough unique cards
-        if(uniqueIds >= pe.count)
+        if(uniqueCards >= pe.count)
         {
             List<CardHolder> cardPoolCopy = new ArrayList<>(cardPool.size());
             cardPoolCopy.addAll(cardPool);
@@ -154,5 +162,22 @@ public class DistributionCardPuller extends CardPuller
     public void addInformation(List<ITextComponent> tooltip)
     {
         this.distribution.addInformation(tooltip, this.set);
+    }
+    
+    public static int countUniqueCards(List<CardHolder> list)
+    {
+        int uniqueCards = 0;
+        Properties lastCard = null;
+        
+        for(CardHolder ch : list)
+        {
+            if(ch.getCard() != lastCard)
+            {
+                lastCard = ch.getCard();
+                ++uniqueCards;
+            }
+        }
+        
+        return uniqueCards;
     }
 }
