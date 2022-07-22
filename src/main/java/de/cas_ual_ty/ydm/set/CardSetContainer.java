ae855945ee799b3695c65b68e1a0f00255ab8b8d
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2022-07-22 18:04:20 +0200

    QOL Part 2

--- src/main/java/de/cas_ual_ty/ydm/set/CardSetContainer.java
+++ src/main/java/de/cas_ual_ty/ydm/set/CardSetContainer.java
@@ -1,6 +1,7 @@
 package de.cas_ual_ty.ydm.set;
 
 import de.cas_ual_ty.ydm.carditeminventory.HeldCIIContainer;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.network.PacketBuffer;
@@ -17,5 +18,23 @@ public class CardSetContainer extends HeldCIIContainer
     public CardSetContainer(ContainerType<?> type, int id, PlayerInventory playerInventoryIn, PacketBuffer extraData)
     {
         super(type, id, playerInventoryIn, extraData);
+    }
+    
+    @Override
+    public void removed(PlayerEntity player)
+    {
+        super.removed(player);
+        
+        for(int i = 0; i < itemHandler.getSlots(); i++)
+        {
+            if(!itemHandler.getStackInSlot(i).isEmpty())
+            {
+                return;
+            }
+        }
+        
+        // item handler is empty
+        // delete empty pack
+        itemStack.shrink(itemStack.getCount());
     }
 }
